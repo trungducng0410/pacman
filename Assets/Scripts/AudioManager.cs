46efe7bb88a534d89dcf709cabe7d2281ffc4efd
Trung Duc <ngtrungducyb@gmail.com>
2021-10-21 20:54:09 +0700

    add game UI appearance

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource startMusic;
     [SerializeField] private AudioSource mainMusic;
 
     // Start is called before the first frame update
     void Start()
     {
-        startMusic.Play();
+        mainMusic.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!startMusic.isPlaying && !mainMusic.isPlaying)
-        {
-            mainMusic.Play();
-        }
 
     }
 }
